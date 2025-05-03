120a84bd8f95f4bc4263aee7fa9129c0d352c565
RamonKaspar <ramonkaspar@gmail.com>
2025-05-03 16:08:18 +0200

    Add logs of token-list and packed training run

---
+++ scripts/run_pretokenized_token_list.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --account=a-large-sc
+#SBATCH --time=00:14:59
+#SBATCH --job-name=lsai_pretokenized_token_list
+#SBATCH --output=/iopsstor/scratch/cscs/%u/project/logs/%x-%j.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=4
+#SBATCH --cpus-per-task=72
+#SBATCH --mem=460000
+#SBATCH --environment=/iopsstor/scratch/cscs/kasparr/project/ngc_pt_jan.toml
+#SBATCH --no-requeue	# Prevent Slurm to requeue the job if the execution crashes (e.g. node failure) so we don't loose the logs
+
+echo "START TIME: $(date)"
+
+# Set up ENV
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+ASSIGNMENT_DIR="/iopsstor/scratch/cscs/$USER/project"
+
+CMD_PREFIX="numactl --membind=0-3"
+
+TRAINING_CMD="python3 $ASSIGNMENT_DIR/src/train.py \
+    --dataset /capstor/scratch/cscs/kasparr/project/train_data_tokenized_token-list.parquet \
+    --dataset-type token-list \
+    --pretokenized \
+    --sequence-length 2048 \
+    --batch-size 1 \
+    --learning-rate 5e-5 \
+    --lr-warmup-steps 100 \
+    --training-steps 1000 \
+    "
+
+srun --cpus-per-task $SLURM_CPUS_PER_TASK bash -c "$CMD_PREFIX $TRAINING_CMD"
+
+echo "END TIME: $(date)"
